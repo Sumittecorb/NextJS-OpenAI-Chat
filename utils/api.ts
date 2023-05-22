d38b4bb92a7643e158efb2864bf78cadc48d975d
Sumit Saxena <sumit.2019@tecorb.co>
2023-05-22 13:15:37 +0530

    Open Ai chat set up

--- utils/api.ts
+++ utils/api.ts
@@ -1,21 +1,24 @@
-import { NEXT_DATA } from "next/dist/shared/lib/utils";
 import { OpenAIApi, Configuration } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey:process.env.NEXT_OPENAI_API_KEY ,
 });
+// console.log("process.env.NEXT_OPENAI_API_KEY",process.env.NEXT_OPENAI_API_KEY);/
 
 const openai = new OpenAIApi(configuration);
 
-export default async (req: any, res: any) => {
-  if (req.body.prompt !== undefined) {
+export const chatCompletion = async (prompt: string) => {
+  if (prompt !== undefined) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${req.body.prompt}`,
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 3000,
     });
-
-    res.status(200).json({ text: `${completion.data.choices[0].text}` });
+    console.log("completion", completion);
+    return completion.data.choices[0].text;
+    // res.status(200).json({ text: `${completion.data.choices[0].text}` });
   } else {
-    res.status(400).json({ text: "No prompt provided." });
+    // res.status(400).json({ text: "No prompt provided." });
   }
 };
